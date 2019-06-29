4f1ef8bc0349ab0e0b8ac03055d084866a56bbe2
SFecho <simonfuecho@163.com>
2019-06-29 19:01:17 +0800

    更新内存管理

--- include/os/memory.h
+++ include/os/memory.h
@@ -3,6 +3,28 @@
 
 #include <types.h>
 
+#define PAGE_GDT_NR 39
+#define PAGE_2M_NR  21
+#define PAGE_1G_NR  30
+#define PAGE_4K_NR  12
+
+#define PAGE_SIZE_4KB   (1UL << PAGE_SIZE_4KB)
+#define PAGE_SIZE_2MB   (1UL << PAGE_2M_NR)
+#define PAGE_SIZE_1GB   (1UL << PAGE_1G_NR)
+
+#define PAGE_ADDR_OFFSET    ((uint64)0xffff800000000000)
+
+#define PAGE_2M_MASK (~(PAGE_SIZE_2MB - 1))
+#define PAGE_1G_MASK (~(PAGE_SIZE_1GB - 1))
+#define PAGE_4K_MASK (~(PAGE_SIZE_4KB - 1))
+
+#define PAGE_2M_ALIGN(addr) (((uint64)(addr) + PAGE_SIZE_2MB - 1) & PAGE_2M_MASK)
+#define PAGE_4K_ALIGN(addr) (((uint64)(addr) + PAGE_SIZE_4KB - 1) & PAGE_4K_MASK)
+#define PAGE_1G_ALIGN(addr) (((uint64)(addr) + PAGE_SIZE_1GB - 1) & PAGE_1G_MASK)
+
+#define virtual_to_physics(addr)    (uint64(addr) - PAGE_ADDR_OFFSET)
+#define virtual_to_physics(addr)    ((uint64*)((uint64)(addr) + PAGE_ADDR_OFFSET))
+
 typedef struct memory_e820 memory_e820;
 typedef struct memory_desc memory_desc;
 
--- mm/memory.c
+++ mm/memory.c
@@ -13,9 +13,34 @@ void init_memory()
         printk("address:%#018lx\tlength:%#018lx\ttype:%#018lx\n", p->addr, p->length, p->type);
         if (p->type == 1)
             total_mem += p->length;
+        memory_manage_struct.e820[i].addr = p->addr;
+        memory_manage_struct.e820[i].length = p->length;
+        memory_manage_struct.e820[i].type = p->type;
+        memory_manage_struct.e820_length = i + 1;
         p++;
         if (p->type > 4)
             break;
     }
+ 
     printk("OS available RAM is %ldB\n", total_mem);
+    
+    total_mem = 0;
+
+    for(i = 0; i < memory_manage_struct.e820_length; i++){
+        uint64 start, end;       
+        if(memory_manage_struct.e820[i].type != 1)
+            continue;
+        start = PAGE_2M_ALIGN(memory_manage_struct.e820[i].addr);
+        end = ((memory_manage_struct.e820[i].addr + memory_manage_struct.e820[i].length) >> PAGE_2M_NR)
+                << PAGE_2M_NR;
+        
+        printk("%#x, %#x\n", start, end);
+       
+        if(end <= start)
+            continue;
+        
+        total_mem += (end-start) >> PAGE_2M_NR;
+   } 
+
+    printk("OS can use 2MB pages is %#010x=%010d\n", total_mem, total_mem);
 }
